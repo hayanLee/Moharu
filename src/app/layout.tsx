3ec00d17e121e1504e30fec01bf029c4a3d1d001
hayanLee <hayanlee.dev@gmail.com>
2025-03-24 01:40:26 +0900

    Chore : 프로젝트명 변경

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,29 +5,29 @@ import localFont from 'next/font/local';
 import './globals.css';
 
 const dynapuff = DynaPuff({
-    subsets: ['latin'],
+  subsets: ['latin'],
 });
 
 const pretendard = localFont({
-    src: '../assets/fonts/PretendardVariable.woff2',
-    variable: '--font-pretendard',
+  src: '../assets/fonts/PretendardVariable.woff2',
+  variable: '--font-pretendard',
 });
 
 export const metadata: Metadata = {
-    title: 'Habit-challenge',
-    description: '사용자가 목표를 달성하며 랜덤 스티커를 수집하고 컬렉션을 완성하는 동기부여 어플리케이션',
+  title: 'Moharu',
+  description: '사용자가 목표를 달성하며 랜덤 스티커를 수집하고 컬렉션을 완성하는 동기부여 어플리케이션',
 };
 
 export default function RootLayout({
-    children,
+  children,
 }: Readonly<{
-    children: React.ReactNode;
+  children: React.ReactNode;
 }>) {
-    return (
-        <html lang='ko' className={`${dynapuff.className} ${pretendard.variable}`}>
-            <body>
-                <Providers>{children}</Providers>
-            </body>
-        </html>
-    );
+  return (
+    <html lang='ko' className={`${dynapuff.className} ${pretendard.variable}`}>
+      <body>
+        <Providers>{children}</Providers>
+      </body>
+    </html>
+  );
 }
