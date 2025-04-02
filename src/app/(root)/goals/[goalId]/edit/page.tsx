122afce3e1e1434c0d59df478a2dc2aed132b89c
hayanLee <hayanlee.dev@gmail.com>
2025-04-02 12:56:05 +0900

    Style : 취소, 완료 버튼 ui 설정 페이지와 동일하게 변경

--- src/app/(root)/goals/[goalId]/edit/page.tsx
+++ src/app/(root)/goals/[goalId]/edit/page.tsx
@@ -3,11 +3,11 @@ import { Button } from '@/components/ui/button';
 import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form';
 import { Input } from '@/components/ui/input';
 import { RadioGroup, RadioGroupItem } from '@/components/ui/radio-group';
+import { HOME } from '@/constant/pathname';
 import useUpdateChallenge from '@/hooks/mutations/useUpdateChallenge';
 import useDetailChallenge from '@/hooks/querys/useDetailChallenge';
 import { cn } from '@/lib/utils';
-import { ArrowLeft } from 'lucide-react';
-import { useRouter } from 'next/navigation';
+import Link from 'next/link';
 import { FieldValues, useForm } from 'react-hook-form';
 import { CATEGORY, PERIODS } from '../_constants/constant';
 
@@ -16,7 +16,6 @@ interface EditPageProps {
 }
 
 const EditPage = ({ params: { goalId } }: EditPageProps) => {
-  const router = useRouter();
   const { mutate } = useUpdateChallenge();
   const { data, isPending } = useDetailChallenge(goalId);
   const form = useForm({
@@ -35,83 +34,82 @@ const EditPage = ({ params: { goalId } }: EditPageProps) => {
     mutate(updateChallegeInfo);
   };
 
-  const goBack = () => router.back();
-
   return (
     <>
-      <Button variant={'ghost'} size={'icon'} asChild>
-        <ArrowLeft onClick={goBack} />
-      </Button>
-
       <Form {...form}>
-        <form className='flex flex-col h-full gap-3' onSubmit={form.handleSubmit(onSubmit)}>
-          <h3 className='text-lg font-semibold sm:text-xl my-3'>챌린지명</h3>
-          <FormField
-            control={form.control}
-            name='challenge_name'
-            rules={{
-              required: '챌린지명은 필수입니다.',
-              maxLength: { value: 20, message: '최대 20자 이내로 작성해주세요' },
-            }}
-            render={({ field }) => (
-              <FormItem>
-                <FormControl>
-                  <Input type='text' placeholder='이름을 입력하세요' required className='py-6' {...field} />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
+        <form className='flex flex-col h-full' onSubmit={form.handleSubmit(onSubmit)}>
+          <div className='flex flex-col gap-2 grow'>
+            <h3 className='text-lg font-semibold sm:text-xl my-3'>챌린지명</h3>
+            <FormField
+              control={form.control}
+              name='challenge_name'
+              rules={{
+                required: '챌린지명은 필수입니다.',
+                maxLength: { value: 20, message: '최대 20자 이내로 작성해주세요' },
+              }}
+              render={({ field }) => (
+                <FormItem>
+                  <FormControl>
+                    <Input type='text' placeholder='이름을 입력하세요' required className='py-6' {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
 
-          <h3 className='text-lg font-semibold sm:text-xl my-3'>기간</h3>
-          <FormField
-            control={form.control}
-            name='period'
-            render={({ field }) => (
-              <div>
-                <div className='grid grid-cols-2 gap-2'>
-                  {PERIODS.map((value) => (
-                    <Button
-                      type='button'
-                      key={value.period}
-                      className={cn('sm:p-3 p-1 border justify-start', value.period === field.value && 'bg-point')}
-                      size={'full'}
-                      variant={'outline'}
-                      value={value.period}
-                      disabled
-                    >
-                      <span className='border rounded-full p-2 bg-gray-200 w-10 h-10 text-center'>{value.icon}</span>
-                      <p className='text-base sm:text-lg'>
-                        <span className='font-semibold'>D-{value.period} </span>
-                      </p>
-                    </Button>
-                  ))}
-                </div>
-              </div>
-            )}
-          />
-
-          <h3 className='text-lg font-semibold sm:text-xl my-3'>카테고리</h3>
-          <FormField
-            control={form.control}
-            name='category'
-            render={({ field }) => (
-              <RadioGroup value={field.value} onValueChange={field.onChange}>
-                {CATEGORY.map((category) => (
-                  <div className='flex items-center space-x-2' key={category.title}>
-                    <RadioGroupItem value={category.title} id={category.title} />
-                    <FormLabel htmlFor={category.title} className={`bg-${category.color}-300 px-2 py-1 rounded`}>
-                      {category.title}
-                    </FormLabel>
+            <h3 className='text-lg font-semibold sm:text-xl my-3'>기간</h3>
+            <FormField
+              control={form.control}
+              name='period'
+              render={({ field }) => (
+                <div>
+                  <div className='grid grid-cols-2 gap-2'>
+                    {PERIODS.map((value) => (
+                      <Button
+                        type='button'
+                        key={value.period}
+                        className={cn('sm:p-3 p-1 border justify-start', value.period === field.value && 'bg-point')}
+                        size={'full'}
+                        variant={'outline'}
+                        value={value.period}
+                        disabled
+                      >
+                        <span className='border rounded-full p-2 bg-gray-200 w-10 h-10 text-center'>{value.icon}</span>
+                        <p className='text-base sm:text-lg'>
+                          <span className='font-semibold'>D-{value.period} </span>
+                        </p>
+                      </Button>
+                    ))}
                   </div>
-                ))}
-              </RadioGroup>
-            )}
-          />
+                </div>
+              )}
+            />
 
-          <Button type='submit' className='text-base mt-10'>
-            수정하기
-          </Button>
+            <h3 className='text-lg font-semibold sm:text-xl my-3'>카테고리</h3>
+            <FormField
+              control={form.control}
+              name='category'
+              render={({ field }) => (
+                <RadioGroup value={field.value} onValueChange={field.onChange}>
+                  {CATEGORY.map((category) => (
+                    <div className='flex items-center space-x-2' key={category.title}>
+                      <RadioGroupItem value={category.title} id={category.title} />
+                      <FormLabel htmlFor={category.title} className={`bg-${category.color}-300 px-2 py-1 rounded`}>
+                        {category.title}
+                      </FormLabel>
+                    </div>
+                  ))}
+                </RadioGroup>
+              )}
+            />
+          </div>
+
+          <div className='grid grid-cols-2 gap-2 p-4'>
+            <Button variant={'outline'} asChild>
+              <Link href={HOME}>취소</Link>
+            </Button>
+            <Button type='submit'>수정</Button>
+          </div>
         </form>
       </Form>
     </>
--- src/app/(root)/goals/new/page.tsx
+++ src/app/(root)/goals/new/page.tsx
@@ -35,76 +35,80 @@ const CreateGoalPage = () => {
 
   return (
     <Form {...form}>
-      <form className='flex flex-col h-full gap-2' onSubmit={form.handleSubmit(onSubmit)}>
-        <h3 className='text-lg font-semibold sm:text-xl my-3'>챌린지명</h3>
-        <FormField
-          control={form.control}
-          name='challengeName'
-          rules={{
-            required: '챌린지명은 필수입니다.',
-            maxLength: { value: 20, message: '최대 20자 이내로 작성해주세요' },
-          }}
-          render={({ field }) => (
-            <FormItem>
-              <FormControl>
-                <Input type='text' placeholder='이름을 입력하세요' className='py-6' {...field} />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
+      <form className='flex flex-col h-full' onSubmit={form.handleSubmit(onSubmit)}>
+        <div className='grow flex flex-col gap-2 '>
+          <h3 className='text-lg font-semibold sm:text-xl my-3'>챌린지명</h3>
+          <FormField
+            control={form.control}
+            name='challengeName'
+            rules={{
+              required: '챌린지명은 필수입니다.',
+              maxLength: { value: 20, message: '최대 20자 이내로 작성해주세요' },
+            }}
+            render={({ field }) => (
+              <FormItem>
+                <FormControl>
+                  <Input type='text' placeholder='이름을 입력하세요' className='py-6' {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
 
-        <h3 className='text-lg font-semibold sm:text-xl my-3'>기간</h3>
-        <FormField
-          control={form.control}
-          name='period'
-          rules={{
-            required: '기간 설정은 필수입니다.',
-          }}
-          render={({ field }) => (
-            <div className='grid grid-cols-2 gap-2'>
-              {PERIODS.map((value) => (
-                <Button
-                  type='button'
-                  key={value.period}
-                  className={cn('sm:p-3 p-1 border justify-start', field.value === value.period && 'bg-point')}
-                  size={'full'}
-                  variant={'outline'}
-                  value={value.period}
-                  onClick={() => field.onChange(value.period)}
-                >
-                  <span className='border rounded-full p-2 bg-gray-200 w-10 h-10 text-center'>{value.icon}</span>
-                  <p className='text-base sm:text-lg'>
-                    <span className='font-semibold'>D-{value.period} </span>
-                  </p>
-                </Button>
-              ))}
-              <FormMessage />
-            </div>
-          )}
-        />
+          <h3 className='text-lg font-semibold sm:text-xl my-3'>기간</h3>
+          <FormField
+            control={form.control}
+            name='period'
+            rules={{
+              required: '기간 설정은 필수입니다.',
+            }}
+            render={({ field }) => (
+              <div className='grid grid-cols-2 gap-2'>
+                {PERIODS.map((value) => (
+                  <Button
+                    type='button'
+                    key={value.period}
+                    className={cn('sm:p-3 p-1 border justify-start', field.value === value.period && 'bg-point')}
+                    size={'full'}
+                    variant={'outline'}
+                    value={value.period}
+                    onClick={() => field.onChange(value.period)}
+                  >
+                    <span className='border rounded-full p-2 bg-gray-200 w-10 h-10 text-center'>{value.icon}</span>
+                    <p className='text-base sm:text-lg'>
+                      <span className='font-semibold'>D-{value.period} </span>
+                    </p>
+                  </Button>
+                ))}
+                <FormMessage />
+              </div>
+            )}
+          />
 
-        <h3 className='text-lg font-semibold sm:text-xl my-3'>카테고리</h3>
-        <FormField
-          control={form.control}
-          name='category'
-          render={({ field }) => (
-            <RadioGroup value={field.value} onValueChange={field.onChange} name='category'>
-              {CATEGORY.map((category) => (
-                <div className='flex items-center space-x-2' key={category.title}>
-                  <RadioGroupItem value={category.title} id={category.title} />
-                  <Label htmlFor={category.title} className={`bg-${category.color}-300 px-2 py-1 rounded`}>
-                    {category.title}
-                  </Label>
-                </div>
-              ))}
-            </RadioGroup>
-          )}
-        />
+          <h3 className='text-lg font-semibold sm:text-xl my-3'>카테고리</h3>
+          <FormField
+            control={form.control}
+            name='category'
+            render={({ field }) => (
+              <RadioGroup value={field.value} onValueChange={field.onChange} name='category'>
+                {CATEGORY.map((category) => (
+                  <div className='flex items-center space-x-2' key={category.title}>
+                    <RadioGroupItem value={category.title} id={category.title} />
+                    <Label htmlFor={category.title} className={`bg-${category.color}-300 px-2 py-1 rounded`}>
+                      {category.title}
+                    </Label>
+                  </div>
+                ))}
+              </RadioGroup>
+            )}
+          />
+        </div>
 
-        <Button type='submit' className='text-base my-10' disabled={!!Object.keys(form.formState.errors).length}>
-          새로운 도전 시작하기 🚩
-        </Button>
+        <div className='p-4'>
+          <Button type='submit' className='w-full' disabled={!!Object.keys(form.formState.errors).length}>
+            새로운 챌린지 시작하기 🚩
+          </Button>
+        </div>
       </form>
     </Form>
   );
