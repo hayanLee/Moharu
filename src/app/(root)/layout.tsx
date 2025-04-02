fc13d88452fb424a480e48a76f3b7fba2da4a678
hayanLee <hayanlee.dev@gmail.com>
2025-04-02 18:23:54 +0900

    Fix: 비로그인 상태에서 쿼리 조회 시 에러 발생 → 로그인 후 최상단 레이아웃에서 처리하도록 수정

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -1,9 +1,18 @@
+'use client';
 import Footer from '@/components/common/Footer';
 import Header from '@/components/common/Header';
 import { Toaster } from '@/components/ui/toaster';
-import { PropsWithChildren } from 'react';
+import useProfile from '@/hooks/querys/useProfile';
+import { PropsWithChildren, useEffect } from 'react';
 
 const MainLayout = ({ children }: PropsWithChildren) => {
+  const { data } = useProfile();
+  useEffect(() => {
+    if (data?.data.color) {
+      document.documentElement.style.setProperty('--point', data.data.color);
+    }
+  }, [data?.data.color]);
+
   return (
     <div className='h-dvh'>
       <div className='max-w-full md:max-w-2xl mx-auto h-full bg-background flex flex-col'>
--- src/providers/ThemeProvider.tsx
+++ src/providers/ThemeProvider.tsx
@@ -1,17 +1,9 @@
 'use client';
 
-import useProfile from '@/hooks/querys/useProfile';
 import { ThemeProvider as NextThemesProvider } from 'next-themes';
 import * as React from 'react';
 
 const ThemeProvider = ({ children, ...props }: React.ComponentProps<typeof NextThemesProvider>) => {
-  const { data } = useProfile();
-  React.useEffect(() => {
-    if (data?.data.color) {
-      document.documentElement.style.setProperty('--point', data.data.color);
-    }
-  }, [data?.data.color]);
-
   return (
     <NextThemesProvider attribute='class' defaultTheme='system' enableSystem {...props}>
       {children}
